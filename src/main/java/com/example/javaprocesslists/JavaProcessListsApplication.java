93ce8abc1e91f4b4ca3ae991cd7f3f25e2a1d467
Cristian Cajiao <cristiancajiaos@outlook.com>
2025-08-19 12:28:45 -0400

    Primer commit

---
+++ src/main/java/com/example/javaprocesslists/JavaProcessListsApplication.java
@@ -0,0 +1,13 @@
+package com.example.javaprocesslists;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaProcessListsApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(JavaProcessListsApplication.class, args);
+  }
+
+}
